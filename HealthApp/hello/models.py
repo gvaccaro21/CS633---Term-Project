b1ac88924a8cc550e0db893ae55390637f2348f8
Mike Smith <mikejmsmith@gmail.com>
2018-02-15 21:29:37 -0500

    creating post for measurments

--- HealthApp/hello/models.py
+++ HealthApp/hello/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class Measurements(models.Model):
+    M_user = models.CharField(max_length=30)
+    M_BMI = models.IntegerField(max_length=3)
+    M_Height = models.IntegerField(max_length=3)
+    M_Weight = models.IntegerField(max_length=3)
--- HealthApp/hello/views.py
+++ HealthApp/hello/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Greeting, Measurements
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
+    # return the Main site page
     return render(request, 'index.html')
 
 def measure(request):
-    # return HttpResponse('Hello from Python!')
+    # return the Measurements Page
     return render(request, 'measure.html')
+
+def savemeasurements(request):
+    if request.method =="POST":
+        user = ""
+        intBMI = request.POST.get("inputBMI", "")
+        intHeight = request.POST.get("inputHeight", "")
+        intWeight = request.POST.get("inputWeight", "")
+        objMeasurments = Measurements(M_user = user, M_BMI = intBMI, M_Height = intHeight, M_Weight = intWeight)
+        objMeasurments.save()   
 
 def db(request):
 
